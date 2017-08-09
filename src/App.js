6c6b2a4f424d71ff6a09a24e041524b2334509c5
paulacurso <paulacursocic@gmail.com>
2017-08-09 18:29:46 +0200

    App acabada

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = this.props;
+    this.change = function(event){
+      let value = event.target.value;
+      switch(event.target.name){
+        case "nombre":
+        if(event.target.value.length>20){
+          ReactDOM.render(<Error mensaje = "La longitud debe ser inferior a 20"/>, document.getElementById('error'));
+        }else{
+          this.setState({persona :{Nombre :value}});
+        }
+        break;
+        case "apellidos":
+        if(event.target.value.length>20){
+          ReactDOM.render(<Error mensaje = "La longitud debe ser inferior a 20"/>, document.getElementById('error'));
+        }else{
+          this.setState({persona :{Apellidos :value}});
+        }
+        break;
+        case "edad":
+        if(event.target.value<0 || event.target.value>120){
+          ReactDOM.render(<Error mensaje = "Debe ser entre 0 y 120"/>, document.getElementById('error'));
+        }else{
+          this.setState({persona :{Edad :value}});
+        }
+        break;
+      }
+    }
+  };
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <h1>Formulario</h1>
+      <p>Nombre: <input type='text' name='nombre' defaultValue={this.state.persona.Nombre} onChange={(e)=>this.change(e)}/></p>
+      <p>Apellidos:<input type='text' name='apellidos' defaultValue={this.state.persona.Apellidos} onChange={(e)=>this.change(e)}/></p>
+      <p>Edad:<input type='number' name='edad' defaultValue={this.state.persona.Edad} onChange={(e)=>this.change(e)}/></p>
       </div>
-    );
+      );
   }
 }
 
+class Error extends Component {
+  render() {
+    return (<p>{this.props.mensaje}</p>);
+  }
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const persona = {
+	Nombre: 'Juan',
+	Apellidos: 'Perez',
+	Edad: 55
+}
+ReactDOM.render(<App persona = {persona}/>, document.getElementById('root'));
 registerServiceWorker();
